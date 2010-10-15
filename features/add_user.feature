b8e2669d73a64799482fc0cb1452d8b52afb17d0
o <o@immerda.ch>
2010-10-15 15:26:19 +0200

    missing adds

---
+++ features/add_user.feature
@@ -0,0 +1,11 @@
+Feature: Adding a User
+
+  Scenario: adding two disks in one keyfile
+    Given there are no permissions set
+    When I run arver in test mode with arguments "-t /location2/machine1 --add-user test"
+    Then file "../spec/data/keys/test/key_000002" is not created
+    And I should see "adduser was called with target /location2/machine1 and user test (slot-no 7)"
+  
+  Scenario: trying to add a wrong user
+    When I run arver in test mode with arguments "-t /location2 --add-user asf"
+    Then I should see "no such user"
---
+++ features/step_definitions/arver_steps.rb
@@ -0,0 +1,21 @@
+Given /^there is a key for all test Partitions/ do
+  `cp spec/data/test_key_000001 spec/data/keys/test/key_000001`
+end
+
+Given /^there are no permissions set/ do
+  `rm -rf spec/data/keys/test/key_*`
+end
+
+When /^I run arver with arguments "(.*)"/ do | arguments|
+  @stdout = File.expand_path(File.join(@tmp_root, "executable.out"))
+  in_project_folder do
+    system "../bin/arver #{arguments} > #{@stdout} 2> #{@stdout}"
+  end
+end
+
+When /^I run arver in test mode with arguments "(.*)"/ do | arguments|
+  @stdout = File.expand_path(File.join(@tmp_root, "executable.out"))
+  in_project_folder do
+    system "../bin/arver -u test -c ../spec/data --test-mode #{arguments} > #{@stdout} 2> #{@stdout}"
+  end
+end
