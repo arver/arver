569150a02b3df602d5be6627895549135e0f640b
mh <mh@immerda.ch>
2010-11-30 21:25:33 +0100

    make this mess a bit more readable before refactoring it

--- lib/arver/info_action.rb
+++ lib/arver/info_action.rb
@@ -15,12 +15,13 @@ module Arver
       caller.execute
       result = caller.output
       a= {}
-      bla = result.each{|s| 
-                a1=s.split(':').compact; 
-                v = '';
-                v = a1[1].strip if not a1[1].nil?;
-                v = v + ':' + a1[2].strip if not a1[2].nil?;
-                a[a1[0].strip] = v if not a1[0].nil? }
+      result.each do |s|
+                a1=s.split(':').compact
+                v = ''
+                v = a1[1].strip if a1[1]
+                v = v + ':' + a1[2].strip if a1[2]
+                a[a1[0].strip] = v if a1[0]
+      end
       filling = 40-partition.device.length
       filling = 0 if filling < 0
       Arver::Log.info( "  #{partition.device}#{' '*filling}: Slots: #{(0..7).map{|i| a['Key Slot '+i.to_s] == 'ENABLED' ? 'X' : '_'}.join}; LUKSv#{a['Version']}; Cypher: #{a['Cipher name']}:#{a['Cipher mode']}:#{a['Hash spec']}; UUID=#{a['UUID']}" )
